f3522adf56853433ffabc872250cb9aa6689854e
mrahman <rahmanww@gmail.com>
2020-07-13 22:19:30 -0400

    modules

---
+++ Amazon/src/test/java/search/DropDownMenu.java
@@ -0,0 +1,20 @@
+package search;
+
+import base.CommonAPI;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class DropDownMenu extends CommonAPI {
+
+    @Test
+    public void dropDown(){
+        List<WebElement> elementList = getListOfWebElements("#searchDropdownBox option");
+        List<String> listOfString = getListOfString(elementList);
+        for(String st:listOfString){
+            System.out.println(st);
+        }
+    }
+}
