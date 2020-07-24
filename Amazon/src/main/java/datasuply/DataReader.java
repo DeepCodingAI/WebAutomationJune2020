7d0f836e0dc37acfde49efadd2c61ca6f21e07b5
mrahman <rahmanww@gmail.com>
2020-07-23 22:51:58 -0400

    database

--- Amazon/src/main/java/datasuply/DataReader.java
+++ Amazon/src/main/java/datasuply/DataReader.java
@@ -1,10 +1,16 @@
 package datasuply;
 
+import base.CommonAPI;
+import datasources.ConnectToSqlDB;
+
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataReader {
 
+    public static ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
     public static List<String> getListOfItems(){
         List<String> list = new ArrayList<String>();
         list.add("java books");
@@ -14,6 +20,12 @@ public class DataReader {
         list.add("honey");
         list.add("Comic Books");
 
+        return list;
+    }
+
+    public static List<String> getItemsListFromDB()throws Exception, IOException, SQLException, ClassNotFoundException {
+        List<String> list = new ArrayList<>();
+        list = connectToSqlDB.readDataBase("ItemList", "items");
         return list;
     }
 }
--- Amazon/src/test/java/search/SearchItems.java
+++ Amazon/src/test/java/search/SearchItems.java
@@ -7,12 +7,14 @@ import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SearchItems extends CommonAPI {
 
     @Test
-    public void test1() throws InterruptedException{
+    public void readHardCodedItems() throws InterruptedException{
         List<String> data = DataReader.getListOfItems();
         for(int i=0; i<data.size(); i++) {
             typeOnElement("#twotabsearchtextbox", data.get(i));
@@ -22,6 +24,16 @@ public class SearchItems extends CommonAPI {
     }
 
     @Test
+    public void readDatabaseItems() throws Exception {
+        List<String> data = DataReader.getItemsListFromDB();
+        for(int i=0; i<data.size(); i++) {
+            typeOnElement("#twotabsearchtextbox", data.get(i));
+            Thread.sleep(2000);
+            clearInputField("#twotabsearchtextbox");
+        }
+    }
+
+    //@Test
     public void test2(){
         String expectedTitle = "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more";
         String actualTitle = driver.getTitle();
