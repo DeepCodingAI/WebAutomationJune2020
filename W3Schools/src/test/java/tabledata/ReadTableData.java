c7b3df6d090fdaf300654ea2d793714264fd2273
mrahman <rahmanww@gmail.com>
2020-07-27 22:30:24 -0400

    commonAPI

---
+++ W3Schools/src/test/java/tabledata/ReadTableData.java
@@ -0,0 +1,14 @@
+package tabledata;
+
+import base.CommonAPI;
+import org.testng.annotations.Test;
+import table.SqlTablePage;
+
+public class ReadTableData extends SqlTablePage {
+
+    @Test
+    public void readData(){
+        readTableData();
+    }
+
+}
